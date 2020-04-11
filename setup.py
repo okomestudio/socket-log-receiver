5772a25df2b3ca54058705eb81225db99cb09da1
Taro Sato <okomestudio@gmail.com>
2020-04-10 23:15:49 -0700

    Update repo name and format code

--- setup.py
+++ setup.py
@@ -7,53 +7,52 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-def find_meta(category, fpath='src/socket_log_receiver/__init__.py'):
+def find_meta(category, fpath="src/socket_log_receiver/__init__.py"):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, fpath), 'r') as f:
+    with codecs.open(os.path.join(here, fpath), "r") as f:
         package_root_file = f.read()
     matched = re.search(
-        r"^__{}__\s+=\s+['\"]([^'\"]*)['\"]".format(category),
-        package_root_file, re.M)
+        r"^__{}__\s+=\s+['\"]([^'\"]*)['\"]".format(category), package_root_file, re.M
+    )
     if matched:
         return matched.group(1)
-    raise Exception('Meta info string for {} undefined'.format(category))
+    raise Exception("Meta info string for {} undefined".format(category))
 
 
 setup(
-    name='socket_log_receiver',
-    description='Socket log server',
-    author=find_meta('author'),
-    author_email=find_meta('author_email'),
-    license=find_meta('license'),
-    version=find_meta('version'),
-    platforms=['Linux'],
+    name="socket_log_receiver",
+    description="Socket log server",
+    author=find_meta("author"),
+    author_email=find_meta("author_email"),
+    license=find_meta("license"),
+    version=find_meta("version"),
+    platforms=["Linux"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Utilities'],
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    url='https://github.com/okomestudio/socket_log_server',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    url="https://github.com/okomestudio/socket-log-server",
     install_requires=[],
     extras_require={
-        'dev': ['coverage>=4.5.1',
-                'mock>=2.0.0',
-                'pytest==3.7.0',
-                'pytest-cov>=2.5.1',
-                'pytest-xprocess>=0.12.1']
-    },
-    entry_points={
-        'console_scripts': [
-            'receiver=socket_log_receiver:main',
+        "dev": [
+            "coverage>=4.5.1",
+            "mock>=2.0.0",
+            "pytest==3.7.0",
+            "pytest-cov>=2.5.1",
+            "pytest-xprocess>=0.12.1",
         ]
-    }
+    },
+    entry_points={"console_scripts": ["receiver=socket_log_receiver:main"]},
 )

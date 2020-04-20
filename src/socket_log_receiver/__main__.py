aeccb7d74e6f4b0a3f797c8104e7f1a7fdb17a53
Taro Sato <okomestudio@gmail.com>
2020-04-19 19:01:41 -0700

    Accept conf file from command line

--- src/socket_log_receiver/__main__.py
+++ src/socket_log_receiver/__main__.py
@@ -8,9 +8,10 @@ from .receivers import serve
 
 def main():
     p = ArgumentParser()
+    p.add_argument("--conf")
     config.add_arguments_to_argparse(p)
     args = p.parse_args()
-    config.prepare_from_argparse(args)
+    config.prepare_from_argparse(args, config_file_arg="conf")
     config.register("log", configure_logging)
     config.register("receiver", serve)
     config.load()
